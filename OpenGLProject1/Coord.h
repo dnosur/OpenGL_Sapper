762dc2b342c6cf8b53f257cd9c190365967dda5c
dnosur <45685967+dnosur@users.noreply.github.com>
2024-10-08 17:53:44 +0300

    feat(Coord): create coord structure

---
+++ OpenGLProject1/Coord.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <windows.h>
+
+struct Coord
+{
+    Coord();
+    Coord(const double X, const double Y);
+    Coord(COORD coord);
+
+    double X, Y;
+
+    bool operator==(const Coord& other) const;
+    bool operator!=(const Coord& other) const;
+
+    bool operator==(const COORD& other) const;
+    bool operator!=(const COORD& other) const;
+
+    Coord& operator=(const Coord& other);
+    Coord& operator=(const COORD& other);
+};
