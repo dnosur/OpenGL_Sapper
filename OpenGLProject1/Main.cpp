6fc4241f77bc2d8c6ec12cf3d68460d6c777b9e9
dnosur <45685967+dnosur@users.noreply.github.com>
2024-10-14 17:53:51 +0300

    feat(Main): add glut

--- OpenGLProject1/Main.cpp
+++ OpenGLProject1/Main.cpp
@@ -1,25 +1,26 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 #include <GLFW/glfw3.h>
 #include "Figures.h"
 #include <iostream>
 
 #include "MainWindow.h"
+#include <GL/glut.h>
 
-//void Sample(Window& window) {
-//    Window* main = &window;
-//    main->MakeContext();
-//    main->Update();
-//}
-
-int main(void)
+int main(int argc, char** argv)
 {
     srand(time(NULL));
+    glutInit(&argc, argv);
     if (!glfwInit())
         return -1;
 
+    glRasterPos2f(1, 1);
 
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '!');
     MainWindow main(Size(1280, 720), "Sapper");
 
     main.MakeContext();
+    main.Initialize();
     main.Update();
 
     return 0;
