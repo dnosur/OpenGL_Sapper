b1984c204c38353d0b40e0d9ef2c42016a763a2d
dnosur <45685967+dnosur@users.noreply.github.com>
2024-10-14 17:53:16 +0300

    feat(KeyboardKey): add keyboard key structure

---
+++ OpenGLProject1/KeyboardKey.h
@@ -0,0 +1,20 @@
+#pragma once
+struct KeyboardKey
+{
+	int key;
+	int action;
+
+	bool pressed;
+	bool clamping;
+
+	bool keyProcessed;
+
+	KeyboardKey();
+	KeyboardKey(int key, int action, bool pressed = false);
+
+	bool operator==(const KeyboardKey& other) const;
+	bool operator!=(const KeyboardKey& other) const;
+
+	KeyboardKey& operator=(const KeyboardKey& other);
+};
+
