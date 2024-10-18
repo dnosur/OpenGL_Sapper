1718c3d0dc1251a7857a8d4907e9ed2ab92e4b65
dnosur <45685967+dnosur@users.noreply.github.com>
2024-10-18 16:51:19 +0300

    change(Figures): use 4f colors

--- OpenGLProject1/Figures.cpp
+++ OpenGLProject1/Figures.cpp
@@ -2,6 +2,7 @@
 
 void drawCircle(float cx, float cy, float r, int num_segments) {
     glBegin(GL_TRIANGLE_FAN);
+    glColor4f(.0f, 1.0f, .0f, 1.0f);
     glVertex2f(cx, cy); 
     for (int i = 0; i <= num_segments; i++)
     {
--- OpenGLProject1/Figures.h
+++ OpenGLProject1/Figures.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <GLFW/glfw3.h>
 #include <corecrt_math_defines.h>
 #include <cmath>
