8e7d0537eeae04f98aeb35bbe31aec949b709453
dnosur <45685967+dnosur@users.noreply.github.com>
2024-10-25 17:46:57 +0300

    change(Main): remove unnececcary libs

--- OpenGLProject1/Main.cpp
+++ OpenGLProject1/Main.cpp
@@ -1,8 +1,10 @@
 #define STB_IMAGE_IMPLEMENTATION
+#define GL_GLEXT_PROTOTYPES
 
-#include <GLFW/glfw3.h>
-#include <GL/glut.h>
 #include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include "Figures.h"
 #include "MainWindow.h"
@@ -16,7 +18,7 @@ int main(int argc, char** argv)
         return -1;
 
     SoundSystem soundSystem;
-    MainWindow main(Size(1280, 720), "Sapper");
+    MainWindow main(Size(1280, 720), "Sapper", Color(.4f, .6f, 0));
 
     main.MakeContext();
     main.Initialize();
